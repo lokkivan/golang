b23ce47ffbfa3f5dfc6142b1758962c2656d4419
Ivan Lokk <lokk.is@dclouds.ru>
2021-03-15 08:42:11 +0700

    Linkedlist  delete all hit

---
+++ Data Strucrures/Linked list/README.md
@@ -0,0 +1,14 @@
+![alt text](https://i1.faceprep.in/Companies-1/difference-between-arrays-and-linked-list.png)
+
+ ## Methoods
+
+- [x] printLIstData
+- [x] addFirst
+- [ ] addLast
+- [ ] getFirst
+- [ ] getLast
+- [ ] set(int index, E element)
+- [ ] search
+- [x] delete first hit
+- [x] delete all hit
+- [ ] revers
--- Data Strucrures/Linked list/main.go
+++ Data Strucrures/Linked list/main.go
@@ -10,18 +10,18 @@ type node struct {
 }
 
 type linkedList struct {
-	head *node
+	head   *node
 	length int
 }
 
-func (l *linkedList) prepend (n *node)  {
+func (l *linkedList) addFirst(n *node) {
 	second := l.head
 	l.head = n
 	l.head.next = second
-	l.length ++
+	l.length++
 }
 
-func (l linkedList) printLIstData()  {
+func (l linkedList) printLIstData() {
 	toPrint := l.head
 	for l.length != 0 {
 		fmt.Printf("%d ", toPrint.data)
@@ -31,7 +31,7 @@ func (l linkedList) printLIstData()  {
 	fmt.Printf("\n")
 }
 
-func (l *linkedList) deleteWithValue(value int)  {
+func (l *linkedList) deleteFirstHit(value int) {
 	if l.length == 0 {
 		return
 	}
@@ -54,21 +54,45 @@ func (l *linkedList) deleteWithValue(value int)  {
 	l.length--
 }
 
-func main()  {
+func (l *linkedList) deleteAllHits(value int) {
+	for {
+		if l.head.data == value {
+			l.head = l.head.next
+			l.length--
+		}
+
+		previousToDelete := l.head
+		for previousToDelete.next.data != value {
+			if previousToDelete.next.next == nil {
+				return
+			}
+			previousToDelete = previousToDelete.next
+		}
+		previousToDelete.next = previousToDelete.next.next
+
+		l.length--
+	}
+}
+
+func main() {
 	mylist := linkedList{}
 	node1 := &node{data: 48}
 	node2 := &node{data: 158}
 	node3 := &node{data: 148}
-	node4 := &node{data: 148}
-	node5 := &node{data: 1148}
-	node6 := &node{data: 13348}
-	mylist.prepend(node1)
-	mylist.prepend(node2)
-	mylist.prepend(node3)
-	mylist.prepend(node4)
-	mylist.prepend(node5)
-	mylist.prepend(node6)
+	node4 := &node{data: 159}
+	node5 := &node{data: 148}
+	node6 := &node{data: 1148}
+	node7 := &node{data: 13348}
+
+	mylist.addFirst(node1)
+	mylist.addFirst(node2)
+
+	mylist.addFirst(node4)
+	mylist.addFirst(node5)
+	mylist.addFirst(node6)
+	mylist.addFirst(node7)
+	mylist.addFirst(node3)
 	mylist.printLIstData()
-	mylist.deleteWithValue(148)
+	mylist.deleteAllHits(148)
 	mylist.printLIstData()
 }
